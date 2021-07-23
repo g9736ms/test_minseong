7deb66930e0de4643c9255bcacd77330fc1d802f
g9736ms <77265345+g9736ms@users.noreply.github.com>
2021-07-23 17:05:21 +0900

    Create aws_info.py

---
+++ s3_update/aws_info.py
@@ -0,0 +1,7 @@
+def info():
+    bucket_name = "버킷이름을 넣어준다"
+    aws_name = "사용자이름"
+    aws_id = "아이디 입력"
+    aws_key = "키 값입력해준다."
+    #그리고 해당 값들을 리턴해준다.
+    return bucket_name, aws_name, aws_id, aws_key
