697308946922c160203169fcca15f162589959d6
g9736ms <77265345+g9736ms@users.noreply.github.com>
2021-07-23 17:50:14 +0900

    Update aws_info.py

--- s3_update/aws_info.py
+++ s3_update/aws_info.py
@@ -1,7 +1,7 @@
 def info():
     bucket_name = "버킷이름을 넣어준다"
-    aws_name = "사용자이름"
-    aws_id = "아이디 입력"
-    aws_key = "키 값입력해준다."
+    aws_name = "버킷에 있는 사용자 디렉터리 입력"
+    aws_id = "엑세스키 입력"
+    aws_key = "시크릿 키를 입력."
     #그리고 해당 값들을 리턴해준다.
     return bucket_name, aws_name, aws_id, aws_key
