5ca97124260ac41112a92dd28532dfa1d911502c
g9736ms <77265345+g9736ms@users.noreply.github.com>
2021-07-26 17:27:37 +0900

    Update mk_nfs.sh

--- bash/mk_nfs.sh
+++ bash/mk_nfs.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
 if [ $# -eq 2 ]; then
-  if [[ ! -d $nfsdir ]]; then
-    mkdir -p $nfsdir
-    echo "$nfsdir ${1}(rw,sync,no_root_squash)" >> /etc/exports
-    if [[ $(systemctl is-enabled nfs) -eq "disabled" ]]; then
-      systemctl enable nfs
-    fi
-     systemctl restart nfs
+  mkdir -p ${1}
+  echo "$nfsdir ${2}(rw,sync,no_root_squash)" >> /etc/exports
+  if [[ $(systemctl is-enabled nfs) -eq "disabled" ]]; then
+    systemctl enable nfs
   fi
+   systemctl restart nfs
 else
   echo "ex) mk_nfs.sh <name> <xxx.xxx.xxx.xxx/xx>"
   exit 0
