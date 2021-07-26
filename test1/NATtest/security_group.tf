49971f7389a50e37403e04acb1fc19e08d72f641
g9736ms <77265345+g9736ms@users.noreply.github.com>
2021-07-26 13:35:32 +0900

    Create security_group.tf

---
+++ test1/NATtest/security_group.tf
@@ -0,0 +1,25 @@
+resource "aws_security_group" "ssh" {
+  name = "allow_ssh_and_http_from_all"
+  description = "Allow SSH and HTTP port from all"
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  vpc_id = aws_vpc.testVPC.id
+}
