96f6a972ac889daf3ad78f76f1125c432aea4034
g9736ms <77265345+g9736ms@users.noreply.github.com>
2021-09-15 17:28:26 +0900

    Create launch.tf

---
+++ terraform_auto_s/launch.tf
@@ -0,0 +1,21 @@
+resource "aws_launch_configuration" "as_conf" {
+  count = "${var.create_if_test == true ? 1 : 0}"
+  name          = "web_config0"
+  image_id      = aws_ami_from_instance.test-AMI.id
+  instance_type = "t2.medium"
+  security_groups = ["sg-04bedc9ce11c80738" ]
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_launch_configuration" "as_conf1" {
+  count = "${var.create_if_test == false ? 1 : 0}"
+  name          = "web_config1"
+  image_id      = aws_ami_from_instance.test-AMI1.id
+  instance_type = "t2.medium"
+  security_groups = ["sg-04bedc9ce11c80738" ]
+  lifecycle {
+    create_before_destroy = true
+  }
+}
