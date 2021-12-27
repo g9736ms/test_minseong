9d69f2952516b98eed0883fa27afad9afa6bd540
g9736ms <77265345+g9736ms@users.noreply.github.com>
2021-12-27 09:08:37 +0900

    Create history_aws.sh

---
+++ bash/history_aws.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+sudo touch /var/log/history.log
+sudo chmod 777 /var/log/history.log
+sudo cat <<EOF > /etc/profile.d/history_collecter.sh
+#!/bin/bash
+HISTTIMEFORMAT="%Y-%m-%d_%H:%M:%S `who am i |awk '{print $1,$5}'` "
+export HISTTIMEFORMAT
+my_test()
+{
+        history >> /var/log/history.log
+        history -c
+}
+trap my_test EXIT
+EOF
