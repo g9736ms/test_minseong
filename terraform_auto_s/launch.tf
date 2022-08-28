fcee80278a68e03719d8d4d50796719427a76890
g9736ms <77265345+g9736ms@users.noreply.github.com>
2022-08-28 21:18:41 +0900

    Update launch.tf

--- terraform_auto_s/launch.tf
+++ terraform_auto_s/launch.tf
@@ -3,7 +3,7 @@ resource "aws_launch_configuration" "as_conf" {
   name          = "web_config0"
   image_id      = aws_ami_from_instance.test-AMI.id
   instance_type = "t2.medium"
-  security_groups = ["sg-04bedc9ce11c80738" ]
+  security_groups = ["sg-" ]
   lifecycle {
     create_before_destroy = true
   }
@@ -14,7 +14,7 @@ resource "aws_launch_configuration" "as_conf1" {
   name          = "web_config1"
   image_id      = aws_ami_from_instance.test-AMI1.id
   instance_type = "t2.medium"
-  security_groups = ["sg-04bedc9ce11c80738" ]
+  security_groups = ["sg-" ]
   lifecycle {
     create_before_destroy = true
   }
